5bb4bf1c44a8f1a1c66bf92cb64fd40535335aee
Aleks <karaeeeani@gmail.com>
2025-08-31 13:00:06 +0300

    added spongebob

--- index.js
+++ index.js
@@ -34,3 +34,22 @@ bodyEl.addEventListener("touchmove", (event) => {
   const yPos = touch.clientY - rect.top;
   createHeart(xPos, yPos);
 });
+
+// Обработка кликов на десктопе и тачей на мобильных устройствах
+bodyEl.addEventListener("click", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanEl.style.height = size + "px";
+  spanEl.style.width = size + "px";
+  spanEl.classList.add('spongebob');
+  
+  bodyEl.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+});
